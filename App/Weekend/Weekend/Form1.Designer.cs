9776a9410efacb7e856528f61de92a6aa42a298e
Jaspercgi <112546481+jasperGrubben@users.noreply.github.com>
2023-09-07 09:39:38 +0200

    Begin Login no database

--- App/Weekend/Weekend/Form1.Designer.cs
+++ App/Weekend/Weekend/Form1.Designer.cs
@@ -32,8 +32,10 @@
             this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.btnInloggen = new System.Windows.Forms.Button();
             this.btnRegisteren = new System.Windows.Forms.Button();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.panel1.SuspendLayout();
+            this.pnlMain = new System.Windows.Forms.Panel();
+            this.btnRegistatie = new System.Windows.Forms.Button();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.pnlMain.SuspendLayout();
             this.SuspendLayout();
             // 
             // ietsToolStripMenuItem
@@ -50,33 +52,49 @@
             // 
             // btnInloggen
             // 
-            this.btnInloggen.Location = new System.Drawing.Point(268, 230);
+            this.btnInloggen.Location = new System.Drawing.Point(0, 0);
             this.btnInloggen.Name = "btnInloggen";
-            this.btnInloggen.Size = new System.Drawing.Size(343, 131);
+            this.btnInloggen.Size = new System.Drawing.Size(75, 23);
             this.btnInloggen.TabIndex = 0;
-            this.btnInloggen.Text = "Inloggen";
-            this.btnInloggen.UseVisualStyleBackColor = true;
-            this.btnInloggen.Click += new System.EventHandler(this.btnInloggen_Click);
             // 
             // btnRegisteren
             // 
-            this.btnRegisteren.Location = new System.Drawing.Point(1067, 230);
+            this.btnRegisteren.Location = new System.Drawing.Point(0, 0);
             this.btnRegisteren.Name = "btnRegisteren";
-            this.btnRegisteren.Size = new System.Drawing.Size(343, 131);
-            this.btnRegisteren.TabIndex = 1;
-            this.btnRegisteren.Text = "Registeren";
-            this.btnRegisteren.UseVisualStyleBackColor = true;
-            this.btnRegisteren.Click += new System.EventHandler(this.btnRegisteren_Click);
+            this.btnRegisteren.Size = new System.Drawing.Size(75, 23);
+            this.btnRegisteren.TabIndex = 0;
             // 
-            // panel1
+            // pnlMain
             // 
-            this.panel1.BackColor = System.Drawing.Color.Gray;
-            this.panel1.Controls.Add(this.btnRegisteren);
-            this.panel1.Controls.Add(this.btnInloggen);
-            this.panel1.Location = new System.Drawing.Point(88, 116);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1684, 600);
-            this.panel1.TabIndex = 2;
+            this.pnlMain.BackColor = System.Drawing.Color.DarkGray;
+            this.pnlMain.Controls.Add(this.btnLogin);
+            this.pnlMain.Controls.Add(this.btnRegistatie);
+            this.pnlMain.ForeColor = System.Drawing.SystemColors.ButtonShadow;
+            this.pnlMain.Location = new System.Drawing.Point(172, 83);
+            this.pnlMain.Name = "pnlMain";
+            this.pnlMain.Size = new System.Drawing.Size(1464, 605);
+            this.pnlMain.TabIndex = 0;
+            this.pnlMain.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlMain_Paint);
+            // 
+            // btnRegistatie
+            // 
+            this.btnRegistatie.Location = new System.Drawing.Point(949, 205);
+            this.btnRegistatie.Name = "btnRegistatie";
+            this.btnRegistatie.Size = new System.Drawing.Size(240, 150);
+            this.btnRegistatie.TabIndex = 0;
+            this.btnRegistatie.Text = "Registeren";
+            this.btnRegistatie.UseVisualStyleBackColor = true;
+            this.btnRegistatie.Click += new System.EventHandler(this.btnRegistatie_Click);
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(247, 205);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(240, 150);
+            this.btnLogin.TabIndex = 1;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
             // 
             // Form1
             // 
@@ -84,10 +102,10 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(1840, 791);
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pnlMain);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.panel1.ResumeLayout(false);
+            this.pnlMain.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -97,7 +115,9 @@
         private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
         private System.Windows.Forms.Button btnInloggen;
         private System.Windows.Forms.Button btnRegisteren;
-        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel pnlMain;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Button btnRegistatie;
     }
 }
 
