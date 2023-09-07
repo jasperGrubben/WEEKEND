2622e348523b41aef32eb90d116a25b6b1ac0f5c
Sjoerd <sjoerd.van.veen@student.gildeopleidingen.nl>
2023-09-07 10:39:22 +0200

    register

--- App/Weekend/Weekend/Form1.Designer.cs
+++ App/Weekend/Weekend/Form1.Designer.cs
@@ -33,12 +33,12 @@
             this.btnInloggen = new System.Windows.Forms.Button();
             this.btnRegisteren = new System.Windows.Forms.Button();
             this.pnlMain = new System.Windows.Forms.Panel();
-            this.btnRegistatie = new System.Windows.Forms.Button();
-            this.btnLogin = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
             this.lblGebruikers = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnRegistatie = new System.Windows.Forms.Button();
             this.pnlMain.SuspendLayout();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
@@ -76,73 +76,82 @@
             this.pnlMain.Controls.Add(this.btnLogin);
             this.pnlMain.Controls.Add(this.btnRegistatie);
             this.pnlMain.ForeColor = System.Drawing.SystemColors.ButtonShadow;
-            this.pnlMain.Location = new System.Drawing.Point(172, 83);
+            this.pnlMain.Location = new System.Drawing.Point(115, 53);
+            this.pnlMain.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pnlMain.Name = "pnlMain";
-            this.pnlMain.Size = new System.Drawing.Size(1464, 605);
+            this.pnlMain.Size = new System.Drawing.Size(976, 387);
             this.pnlMain.TabIndex = 0;
             this.pnlMain.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlMain_Paint);
-            // 
-            // btnRegistatie
-            // 
-            this.btnRegistatie.Location = new System.Drawing.Point(949, 205);
-            this.btnRegistatie.Name = "btnRegistatie";
-            this.btnRegistatie.Size = new System.Drawing.Size(240, 150);
-            this.btnRegistatie.TabIndex = 0;
-            this.btnRegistatie.Text = "Registeren";
-            this.btnRegistatie.UseVisualStyleBackColor = true;
-            this.btnRegistatie.Click += new System.EventHandler(this.btnRegistatie_Click);
-            // 
-            // btnLogin
-            // 
-            this.btnLogin.Location = new System.Drawing.Point(247, 205);
-            this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(240, 150);
-            this.btnLogin.TabIndex = 1;
-            this.btnLogin.Text = "Login";
-            this.btnLogin.UseVisualStyleBackColor = true;
-            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
             // 
             // panel1
             // 
             this.panel1.Controls.Add(this.lblGebruikers);
             this.panel1.Controls.Add(this.textBox2);
             this.panel1.Controls.Add(this.textBox1);
-            this.panel1.Location = new System.Drawing.Point(387, 14);
+            this.panel1.Location = new System.Drawing.Point(258, 9);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(673, 572);
+            this.panel1.Size = new System.Drawing.Size(449, 366);
             this.panel1.TabIndex = 1;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(282, 110);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(217, 31);
-            this.textBox1.TabIndex = 0;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(282, 176);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(217, 31);
-            this.textBox2.TabIndex = 1;
+            this.panel1.Visible = false;
             // 
             // lblGebruikers
             // 
             this.lblGebruikers.AutoSize = true;
             this.lblGebruikers.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.lblGebruikers.Location = new System.Drawing.Point(117, 116);
+            this.lblGebruikers.Location = new System.Drawing.Point(78, 74);
+            this.lblGebruikers.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblGebruikers.Name = "lblGebruikers";
-            this.lblGebruikers.Size = new System.Drawing.Size(165, 25);
+            this.lblGebruikers.Size = new System.Drawing.Size(103, 16);
             this.lblGebruikers.TabIndex = 2;
             this.lblGebruikers.Text = "Gebruikernaam:";
             // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(188, 113);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(146, 22);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(188, 70);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(146, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(165, 131);
+            this.btnLogin.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(160, 96);
+            this.btnLogin.TabIndex = 1;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // btnRegistatie
+            // 
+            this.btnRegistatie.Location = new System.Drawing.Point(633, 131);
+            this.btnRegistatie.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnRegistatie.Name = "btnRegistatie";
+            this.btnRegistatie.Size = new System.Drawing.Size(160, 96);
+            this.btnRegistatie.TabIndex = 0;
+            this.btnRegistatie.Text = "Registeren";
+            this.btnRegistatie.UseVisualStyleBackColor = true;
+            this.btnRegistatie.Click += new System.EventHandler(this.btnRegistatie_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(1840, 791);
+            this.ClientSize = new System.Drawing.Size(1227, 506);
             this.Controls.Add(this.pnlMain);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.pnlMain.ResumeLayout(false);
